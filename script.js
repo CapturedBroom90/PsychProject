b12d0b67eb5a3b8ec35546bdac109e8661b7bc82
David Acevedo <58893468+daceCS@users.noreply.github.com>
2023-10-08 22:10:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,27 @@ let wheelSpeed = rootStyles.getPropertyValue("--wheel-speed");
 
 function changeSpeed() {
   speed = document.getElementById("speed-slider").value;
-  root.style.setProperty("--wheel-speed", speed / 10 + "s");
+  if (speed == 10) {
+    root.style.setProperty("--wheel-speed", "0.1s");
+  } else if (speed == 9) {
+    root.style.setProperty("--wheel-speed", "0.2s");
+  } else if (speed == 8) {
+    root.style.setProperty("--wheel-speed", "0.4s");
+  } else if (speed == 7) {
+    root.style.setProperty("--wheel-speed", "0.6s");
+  } else if (speed == 6) {
+    root.style.setProperty("--wheel-speed", "0.7s");
+  } else if (speed == 5) {
+    root.style.setProperty("--wheel-speed", "0.8s");
+  } else if (speed == 4) {
+    root.style.setProperty("--wheel-speed", "0.9s");
+  } else if (speed == 3) {
+    root.style.setProperty("--wheel-speed", "1s");
+  } else if (speed == 2) {
+    root.style.setProperty("--wheel-speed", "1.4s");
+  } else if (speed == 1) {
+    root.style.setProperty("--wheel-speed", "2s");
+  }
+
   console.log(speed);
 }
